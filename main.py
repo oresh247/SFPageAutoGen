e1c3bea71b5dee9f2cfe2ced5bb51223bc3e9abc
oresh247 <oresh247@gmail.com>
2024-08-04 21:15:51 +0300

    Поиск версии еДТО

--- main.py
+++ main.py
@@ -38,7 +38,15 @@ sferaUrlSkmbRepos = config["SFERA"]["sferaUrlSkmbRepos"]
 session = requests.Session()
 session.post(sferaUrlLogin, json={"username": devUser, "password": devPassword}, verify=False)
 
+
 def get_release_tasks(release):
+    """
+    Функция возвращает список задач релиза в виде массива json
+    :param release: str
+         Номер релиза
+    :return json.loads(response.text): str
+        Текст ответа на запрос в формате json
+    """
     # Формируем запрос
     query = 'label%20%3D%20%27' + release + '%27&size=1000&page=0&attributesToReturn=checkbox%2Cnumber%2Cname%2CactualSprint%2Cpriority%2Cstatus%2Cassignee%2Cowner%2CdueDate%2Clabel%2CparentNumber%2Ccomponent'
     url = sferaUrlSearch + "?query=" + query
@@ -50,6 +58,13 @@ def get_release_tasks(release):
 
 
 def get_task_comments(task_number):
+    """
+    Функция возвращает массив комментариев к задаче в виде json массива
+    :param task_number: str
+        Номер задачи
+    :return: str
+        Текст ответа на запрос в формате json
+    """
     # Формируем запрос
     path = '/attributes/comments?'
     query = 'sort=createDate,desc&size=1000'
@@ -59,6 +74,7 @@ def get_task_comments(task_number):
     if response.ok != True:
         raise Exception("Error get sprint data " + response)
     return json.loads(response.text)
+
 
 def generate_release_html(tasks_df):
     # Генерируем HTML-код
@@ -106,6 +122,7 @@ def get_prod_versions(file_path):
     prod.drop(columns=['data'], inplace=True)
     return prod
 
+
 def get_file_edto_name(file_path):
     file_names = pd.read_csv(file_path, header=None, names=['data'])
 
@@ -122,20 +139,25 @@ def get_file_edto_name(file_path):
     file_names.drop(columns=['data'], inplace=True)
     return file_names
 
-def formation_of_lists(tasks, release, prod, edto_file_names):
-    component_lst = []
-    task_directLink_lst = []
-    prod_version_lst = []
-    task_lst = []
-    inventory_changed_lst = []
-    edto_lst = []
 
+def formation_of_lists(tasks, release, prod, edto_file_names):
+    component_lst = [] # Список микросервисов релиза
+    task_directLink_lst = [] # Список строк HTML для добавления макроса в таблицу по каждому микросервису
+    prod_version_lst = []  # Список версий ПРОДа по каждому микросервису
+    task_lst = [] # Список номеров задач релиза по каждому микросервису
+    inventory_changed_lst = [] # Список изменений инвентари по каждому микросервису
+    inventory_changed_dic = {}
+    edto_lst = [] # Список версий еДТО по каждому микросервису
+    edto_dic = {}
 
 
     counter = 10 # Начальный счетчик макросов на странице
+    # Перебираем массив задач релиза
     for task in tasks['content']:
         new_task = task['number']
-        service_build = ''
+        component_name = ''
+        text = ''
+        service_build = '' # Предварительно обнуляем номер сборки для задачи
         if new_task not in task_lst:
             task_lst.append(new_task)
 
@@ -144,6 +166,8 @@ def formation_of_lists(tasks, release, prod, edto_file_names):
                 component_name = component['name']
                 if component_name not in component_lst:
                     component_lst.append(component['name'])
+                    inventory_changed_dic[component_name] = ''
+                    edto_dic[component_name] = ''
                     template = f"""
             <p>  <macro class=is-locked data-name=SferaTasks id=SferaTasks-mce_{counter} contenteditable=false    data-rtc-uid=16cce9cf-5572-4a48-a85b-3375c3c8ed6d><macro-parameter data-name=query      data-rtc-uid=d2a03405-badc-4db9-b558-c63defb0c191>label = '{release}' and      component='{component_name}'</macro-parameter><macro-parameter data-name=name      data-rtc-uid=299855c1-a21a-4a91-88b3-99539008e3c6>{release}_{component_name}</macro-parameter><macro-parameter      data-name=maxTasks data-rtc-uid=4a9ba1c3-6e18-4939-a108-7890b7347054>20</macro-parameter><macro-parameter      data-name=attributes      data-rtc-uid=150ff77e-2639-48fa-bba0-32dd06b4104f>[{{'name':'Ключ','code':'number'}},{{'name':'Название','code':'name'}},{{'name':'Статус','code':'status'}},{{'name':'Исполнитель','code':'assignee'}}]</macro-parameter><macro-parameter      data-name=createdDate      data-rtc-uid=05fa3e33-7799-4ddc-bc7e-316a518aeeaa>1716579558662</macro-parameter><macro-parameter      data-name=isLocked      data-rtc-uid=3ba10cb8-4865-45c1-aae9-0e8c5437f9c8>false</macro-parameter><macro-rich-text      data-rtc-uid=5dfe052c-765d-4974-8d5d-4d3e356f9bd9></macro-rich-text></macro></p>
             """
@@ -155,43 +179,42 @@ def formation_of_lists(tasks, release, prod, edto_file_names):
                         prod_version_lst.append(version)
                     else:
                         prod_version_lst.append('')
-
-                    # if component_name in prod['service'].values:
-                    #     version = prod.loc[prod['service'] == component_name, 'version'].values[0]
-                    #     prod_version_lst.append(version)
-                    # else:
-                    #     prod_version_lst.append('')
         else:
             print("Нет компоненты: ",task['number'])
 
-        # if 'label' in task:
-        #     for label in task['label']:
-        #         if label['name'] == 'ОКР_изменение_инвентори':
-        comments = get_task_comments(new_task)
-        if 'content' in comments:
-            text = ''
-            for comment in comments['content']:
-                comment_text = comment['text']
-                if '#Инвентори' in comment_text:
-                    text = text + '<br>' + comment_text
-                    text = text.replace('#Инвентори', '')
-                    text = text.replace('\n', '<br>')
-                if service_build == '':
-                    if '#build' in comment_text:
-                        service_build = str.split(comment_text)[-1]
 
-            if text == '':
-                inventory_changed_lst.append('')
-            else:
-                inventory_changed_lst.append('Изменение инвентори:\n' + text)
+        # Учитываем сборку и инвентари, если указан микросервис в задаче
+        if component_name != '':
+            # Обрабатываем комментарий
+            comments = get_task_comments(new_task)
+            if 'content' in comments:
+                for comment in comments['content']:
+                    comment_text = comment['text']
+                    if '#Инвентори' in comment_text:
+                        text = text + '<br>' + comment_text
+                        text = text.replace('#Инвентори', '')
+                        text = text.replace('\n', '<br>')
+                    if service_build == '':
+                        if '#build' in comment_text:
+                            service_build = str.split(comment_text)[-1]
 
-        if service_build != '':
-            service_edto_version = get_edto_version(component_name, service_build, edto_file_names)
-            edto_lst.append(service_edto_version)
-        else:
-            edto_lst.append('')
+            # Если есть изменения инвентари
+            if text != '':
+                if inventory_changed_dic[component_name] != '':
+                    inventory_changed_dic[component_name] = inventory_changed_dic[component_name] + '\n' + text
+                else:
+                    inventory_changed_dic[component_name] ='Изменение инвентори:\n' + text
 
-    return component_lst, task_directLink_lst, prod_version_lst, task_lst, inventory_changed_lst, edto_lst
+            # Если есть номер сборки
+            if service_build != '':
+                service_edto_version = get_edto_version(component_name, service_build, edto_file_names)
+                if service_edto_version != '':
+                    if component_name in inventory_changed_dic:
+                        edto_dic[component_name] = edto_dic[component_name] + '\n' + service_edto_version
+                    else:
+                        edto_dic[component_name] = service_build
+
+    return component_lst, task_directLink_lst, prod_version_lst, task_lst, list(inventory_changed_dic.values()), list(edto_dic.values())
 
 
 def find_dto_version(text):
@@ -201,6 +224,7 @@ def find_dto_version(text):
     if match:
         return match.group(1)  # Возвращаем версию
     return ''  # Если не найдено
+
 
 def get_edto_version(component_name, service_build, edto_file_names):
     path = '/file/raw/'
@@ -221,10 +245,14 @@ def get_edto_version(component_name, service_build, edto_file_names):
         return ''
     return find_dto_version(response.text)
 
+
 def create_df(component_lst, task_directLink_lst, prod_version_lst, new_version, inventory_changed_lst, edto_lst):
     # Проверка на пустоту списка inventory_changed_lst
     if not inventory_changed_lst:
         inventory_changed_lst = [''] * len(component_lst)  # Заполнение пустыми строками, если список пустой
+    # Проверка на пустоту списка inventory_changed_lst
+    if not edto_lst:
+        edto_lst = [''] * len(component_lst)  # Заполнение пустыми строками, если список пустой
 
     tasks_df = pd.DataFrame({
         'Сервис': component_lst,
@@ -253,9 +281,6 @@ def generating_release_page(parent_page, release, new_version, for_publication_f
 
     # Обрабатываем запрос, проходя по всем задачам и формируя списки
     component_lst, task_directLink_lst, prod_version_lst, task_lst, inventory_changed_lst, edto_lst = formation_of_lists(tasks, release, prod, edto_file_names)
-
-    # Получаем версию еДТО
-
 
     # Создаем dataframe
     tasks_df = create_df(component_lst, task_directLink_lst, prod_version_lst, new_version, inventory_changed_lst, edto_lst)
@@ -371,7 +396,6 @@ def createSferaTask(release):
     return json.loads(response.text)
 
 
-# Генерация страницы ЗНИ с QL выборками
 # Задаем константы
 parent_page = '1295831'
 release = 'OKR_20240908_IR' # Метка релиза
@@ -379,6 +403,7 @@ new_version = '2403.6.0' # Текущая версия сервиса
 story = 'SKOKR-6384'
 for_publication_flg = True # Если True - то публикуем, если False, только возврат списка задач
 
+# Генерация страницы ЗНИ с QL выборками
 task_lst = generating_release_page(parent_page, release, new_version, for_publication_flg)
 if story != '':
     add_task_to_story(task_lst, story)
